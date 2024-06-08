160fdafcba182340c1f162db2f5a67fdc0694011
evahuertas <evahuegar@gmail.com>
2024-06-08 14:02:25 +0200

    UF1842 : 3-APIs

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,10 @@
 async function getPuzzle(wordCount) {
     let hiddenSentence = "Buena Suerte";
+    url = "https://puzzle.mead.io/puzzle";
 
+    wordCount = Math.round(Math.random());
+    const response = await fetch(`${url}?=wordCount${wordCount}`);
+    const data = response.json();
+    console.log("data log : ",data) //promise?
     return hiddenSentence;
 }
