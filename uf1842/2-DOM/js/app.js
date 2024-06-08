bd32475408ce076cb02cd27352fd3056c0680cac
evahuertas <evahuegar@gmail.com>
2024-06-08 12:36:55 +0200

    UF1842 : 2-DOM

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,14 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+var btnEnviar = document.querySelector('#btn-send')
+
+btnEnviar.addEventListener('click', function (e) {
+    var inputMessage = document.querySelector('#message').value
+    console.log(" input : " , inputMessage)
+
+    var lastMessage = document.querySelector('#last-message');
+    lastMessage.textContent = inputMessage;
+
+    document.querySelector('#message').value = '';
+})
